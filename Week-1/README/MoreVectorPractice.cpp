35ae03c8d7ac2c883e2253d712e373bd36c8d8c1
Randol Cespedes <Randolc504@gmail.com>
2025-09-10 10:38:05 -0400

    Add files via upload

---
+++ Week-1/README/MoreVectorPractice.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+/*	
+	vector<int> numbers = {10,20,30,40,50};
+	
+	cout << "First element: " << numbers.front() << endl;
+	cout << "Last element: " << numbers.back() << endl;
+	cout << "Second element: " << numbers[1] << endl;
+	cout << "Third element: " << numbers.at(2) << endl;
+	
+	numbers[1] = 35;
+	numbers.at(2) = 35;
+	numbers.front() = 5;
+	numbers.back() = 5;
+	numbers.push_back(272);
+	
+	cout << "\n\n======After Modifications=====\n\n";
+	
+	cout << "First element: " << numbers.front() << endl;
+	cout << "Last element: " << numbers.back() << endl;
+	cout << "Second element: " << numbers[1] << endl;
+	cout << "Third element: " << numbers.at(2) << endl;
+	
+	return 0;
+*/
+
+	vector<int> numbers;
+	
+	numbers.reserve(2);
+	
+	cout << "First El = " << numbers.front() << endl;
+	cout << "Last El = " << numbers.back() << endl;
+	
+	cout << "Reserved: " << endl << "Size = "  << numbers.size() << ", Capacity = " << numbers.capacity() << endl << endl;
+	
+	numbers.push_back(10);
+	cout << "After adding 10: " << endl << "Size = " << numbers.size() << ", Capacity = " << numbers.capacity() << endl;
+	
+	cout << "First El = " << numbers.front() << endl;
+	cout << "Last El = " << numbers.back() << endl << endl;
+	
+	numbers.push_back(20);
+	cout << "After adding 20: " << endl << "Size = " << numbers.size() << ", Capacity = " << numbers.capacity() << endl;
+	
+	cout << "First El = " << numbers.front() << endl;
+	cout << "Last El = " << numbers.back() << endl << endl;
+	
+	numbers.push_back(40);
+	cout << "After adding 40: " << endl << "Size = " << numbers.size() << ", Capacity = " << numbers.capacity() << endl;
+	
+	cout << "First El = " << numbers.front() << endl;
+	cout << "Last El = " << numbers.back() << endl << endl;
+	
+	
+	return 0;
+}
