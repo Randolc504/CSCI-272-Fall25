18af54638228fe11a3c57c26fa9089781b115bb9
Randol Cespedes <Randolc504@gmail.com>
2025-10-15 10:36:54 -0400

    Add files via upload

---
+++ Week-1/README/StringStreamPrac.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+main(){
+	string input;
+	int number;
+	
+	cout << "Enter an integer between 0 and 100: ";
+	
+	cin >> input;
+	
+	istringstream iss(input); // created a stream from a string (input)
+	
+	if (iss >> number){
+		if (number >= 0 && number <= 100){
+			cout << "Valid Input: " << number << endl;
+		}
+		else
+			cout << number << " Out of Range!\n";
+			
+	} else {
+		cout << "INVALID Input (Not an integer)\n";
+	}
+	return 0;
+}
